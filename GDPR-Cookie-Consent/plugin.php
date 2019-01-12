2b496f36df8439d316aae11edf54916f13a2f34d
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2019-01-13 00:57:31 +0100

    Beutifying things a bit

--- GDPR-Cookie-Consent/plugin.php
+++ GDPR-Cookie-Consent/plugin.php
@@ -11,15 +11,30 @@ class KokenCommunityGDPR extends KokenPlugin {
 	}
 
 	function renderFirst() {
-		echo '<style id="gdpr_css_var">:root {--gdpr-bkgrd:'. $this->data->gdprBkgrd .';--gdpr-text-clr:'. $this->data->gdprTextClr .';--gdpr-consent-clr:'. $this->data->consentClr .';--gdpr-decline-clr:'. $this->data->declineClr .';}</style>';
+		echo '<style id="gdpr_css_var">:root {--gdrp-backdrop:'. $this->data->gdprBackdrop .';--gdpr-header-clr:'. $this->data->headLineClr .';--gdpr-ok-topclr:'. $this->data->consentTopClr .';--gdpr-ok-endclr:'. $this->data->consentEndClr .';--gdpr-ok-brdclr:'. $this->data->consentBrdClr .';--gdpr-bkgrd:'. $this->data->gdprBkgrd .';--gdpr-text-clr:'. $this->data->gdprTextClr .';--gdpr-consent-clr:'. $this->data->consentClr .';--gdpr-decline-clr:'. $this->data->declineClr .';}</style>';
 		echo '<link id="gdpr_css" rel="stylesheet" type="text/css" href="'. $this->get_path() .'/css/gdpr.min.css" />';
 		echo '<script id="gdpr_options">const activeForAll='.$this->data->activateAll.';const ignoredCookie=\''.$this->data->ignoredCookie.'\';</script>';
 		echo '<script id="gpdr_head" src="'. $this->get_path() .'/js/cm-head.min.js"></script>';
 	}
 
 	function renderLast() {
-		echo '<script id="gdpr_options_body">var infoText="'. $this->data->infoText .'";var privacyText="'. $this->data->privacyText .'";var privacyLink="'. $this->data->privacyLink .'";var buttonOk="'. $this->data->buttonConsent .'";var buttonDecline="'. $this->data->buttonDecline .'";</script>';
-		echo '<script id="gdpr_foot" src="'. $this->get_path() .'/js/cm-body.min.js"></script>';
+		//print_r($_COOKIE);
+		$this->site_data->title = Koken::out('site.title');
+		//if(!isset($_COOKIE['CM_cookieConsent']) && $_COOKIE['CM_cookieConsent'] !=='1' ){
+			echo <<<HTML
+<div id="gdpr_alert">
+	<img class="ue-image" src="{$this->get_path()}/eu-flag.svg" />
+	<h1>{$this->data->headLine} {$this->site_data->title}</h1>
+	<p>{$this->data->infoTextOne}<br />{$this->data->infoTextTwo}<br />{$this->data->infoTextThree} <a href="{$this->data->privacyLink}" title="{$this->data->privacyText}">{$this->data->privacyText}</a>.</p>
+	<div class="gdpr-buttons">
+		<button id="consent" class="button gdpr" title="{$this->data->buttonConsent}">{$this->data->buttonConsent}</button>
+		<button id="decline" class="button gdpr" title="{$this->data->buttonDecline}">{$this->data->buttonDecline}</button>
+	</div>
+	<div class="copy" title="GDPR Cookie Consent for Koken by Koken Community Support"><a id="kcs" onclick="return !window.open(this.href);" href="https://kokensupport.com" target="_blank"><img class="gdpr-image" src="{$this->get_path()}/eu-flag.svg" />GDPR Cookie Consent for Koken</a></div>
+</div>
+<script id="gdpr_foot" src="{$this->get_path()}/js/cm-body.min.js"></script>
+HTML;
+		//}
 	}
 }
 
